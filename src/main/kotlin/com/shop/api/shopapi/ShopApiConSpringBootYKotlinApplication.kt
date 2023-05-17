d004589b95b7e5344c850d285d60d0e4cea1b9b7
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-05-17 22:55:44 +0200

    first commit

---
+++ src/main/kotlin/com/shop/api/shopapi/ShopApiConSpringBootYKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.shop.api.shopapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShopApiConSpringBootYKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShopApiConSpringBootYKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/shop/api/shopapi/ShopApiConSpringBootYKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.shop.api.shopapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShopApiConSpringBootYKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
